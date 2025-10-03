aa6237ab2bf872751ade563342706245f17e68ca
Thomas Meerpohl <thomas.meerpohl@gmail.com>
2025-10-03 13:25:42 +0200

    Enable prefetching. Use fonts api.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   // output: 'hybrid'
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "viewport",
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Fredoka",
+        cssVariable: "--font-fredoka",
+        weights: [300, 400, 500, 600, 700],
+      },
+    ],
+  },
 });
